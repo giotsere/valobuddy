53499e4fad4a046d8d9b027737be5cae1d7fd072
giotsere <giorgitsereteli@protonmail.com>
2022-10-13 18:46:55 +0300

    changed /posts to /browse

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Browse from './pages/Browse';
 import Navbar from './components/Navbar';
 import CreatePost from './components/CreatePost';
 import DeletePost from './components/DeletePost';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
@@ -13,8 +15,10 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/posts" element={<Browse />} />
+            <Route path="/browse" element={<Browse />} />
             <Route path="/create" element={<CreatePost />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="/api/posts/:id/edit" element={<CreatePost />} />
             <Route path="/api/posts/:id/delete" element={<DeletePost />} />
           </Routes>
