b6c78ca6d8cf0b14f527c242da5a8d30cc37e95d
giotsere <giorgitsereteli@protonmail.com>
2022-10-13 19:45:16 +0300

    set up reg controller

--- backend/routes/registration.js
+++ backend/routes/registration.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const registration_controller = require('../controllers/registration_controller');
 
-router.get('/signup', registration_controller.signup_get);
-router.post('/signup', registration_controller.signup_post);
-
-router.get('/login', registration_controller.login_get);
 router.post('/login', registration_controller.login_post);
+
+router.post('/signup', registration_controller.signup_post);
 
 module.exports = router;
