26d38dc1b3c1e80dd916e4f82e3187b1eff39180
giotsere <giorgitsereteli@protonmail.com>
2022-10-22 00:59:51 +0300

    passport authennticate middleware working

--- backend/routes/registration.js
+++ backend/routes/registration.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const registration_controller = require('../controllers/registration_controller');
 const authController = require('../auth/authMiddleware');
+const passport = require('passport');
 
-router.post('/login', registration_controller.login_post);
+router.post(
+  '/login',
+  passport.authenticate('local'),
+  registration_controller.login_post
+);
 
 router.post('/signup', registration_controller.signup_post);
 
