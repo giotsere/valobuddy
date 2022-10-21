5ee094a12136f0aca33738850b0b465824d8dd44
giotsere <giorgitsereteli@protonmail.com>
2022-10-21 22:56:42 +0300

    isAuth hook

--- backend/routes/registration.js
+++ backend/routes/registration.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const registration_controller = require('../controllers/registration_controller');
+const authController = require('../auth/authMiddleware');
 
 router.post('/login', registration_controller.login_post);
 
 router.post('/signup', registration_controller.signup_post);
+
+router.post(
+  '/logout',
+  authController.isAuth,
+  registration_controller.logout_post
+);
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const users_controller = require('../controllers/users_controller');
+const authController = require('../auth/authMiddleware');
 
 router.get('/:id', users_controller.user_details);
 
-router.post('/:id/update', users_controller.user_update);
+router.post('/:id/update', authController.isAuth, users_controller.user_update);
 
-router.post('/:id/delete', users_controller.user_delete);
+router.post('/:id/delete', authController.isAuth, users_controller.user_delete);
 
 module.exports = router;
