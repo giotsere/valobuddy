5df6623bc87cd45576af60ace7e7ba2a2c8f8f19
giotsere <giorgitsereteli@protonmail.com>
2022-10-24 22:28:11 +0300

    fixed url

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,18 +1,21 @@
 import { useAuthContext } from '../hooks/useAuthContext';
+import { useNavigate } from 'react-router-dom';
 
 export const useLogout = () => {
+  const navigate = useNavigate();
   const { dispatch } = useAuthContext();
 
   const logoutUser = async () => {
-    console.log(1);
-    localStorage.removeItem('user');
-
-    dispatch({ type: 'LOGOUT' });
-
-    const res = await fetch('api/registration/logout', {
+    const res = await fetch('/api/registration/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
+
+    if (res.ok) {
+      localStorage.removeItem('user');
+      dispatch({ type: 'LOGOUT' });
+      return navigate('/browse');
+    }
   };
 
   return { logoutUser };
