6da2eeeebec5a6610b1201380fcd655fc27e0c8e
giotsere <giorgitsereteli@protonmail.com>
2022-10-26 00:23:25 +0300

    added username field to model

--- backend/models/UserDetail.js
+++ backend/models/UserDetail.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 let UserDetailSchema = new Schema({
   userID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  username: { type: String, required: true },
   avatar: { type: String, data: Buffer, required: false },
   discord: { type: String, required: false },
   riot: { type: String, required: false },
