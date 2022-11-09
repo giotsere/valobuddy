f5c396ff97037e1503caa79ace8c58243cfefd6b
giotsere <giorgitsereteli@protonmail.com>
2022-11-09 22:54:03 +0200

    Extracted post fetching for delete from Post comp into a hook

---
+++ frontend/src/hooks/useFetchDeletePost.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export const useFetchDeletePost = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState('Fetching Post...');
+
+  const fetchDeletePost = async (id) => {
+    const res = await fetch(`/api/posts/${id}/delete`);
+    const data = await res.json();
+
+    if (!res.ok) {
+      setError('Failed to fetch Post...');
+      setLoading(null);
+    }
+
+    if (res.ok) {
+      setLoading(null);
+      setError(null);
+      return data;
+    }
+  };
+  return { error, setLoading, loading, fetchDeletePost };
+};
