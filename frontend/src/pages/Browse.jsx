f667320402e6547f8fef2e7c338c0c55627d7a5f
giotsere <giorgitsereteli@protonmail.com>
2022-12-04 04:38:31 +0200

    pagination, pagination when filtering, modified fetchPosts to use with filtering

--- frontend/src/pages/Browse.jsx
+++ frontend/src/pages/Browse.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Post from '../components/Post';
 import { useAuthContext } from '../hooks/useAuthContext';
@@ -9,48 +9,83 @@ function Browse() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const { user } = useAuthContext();
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
 
-  const handleSubmit = async (filterState, e) => {
-    e.preventDefault();
-    console.log(filterState);
-    let res = await fetch('/api/posts/filtered', {
+  //const [isFiltered, setIsFiltered] = useState(false);
+  // const handleSubmit = async (filterState, e) => {
+  //   e.preventDefault();
+  //   let newFilterState = filterState;
+
+  //   if (isFiltered) {
+  //     newFilterState = {
+  //       ranks: filterState.ranks,
+  //       regions: filterState,
+  //       roles: filterState.roles,
+  //       page: page,
+  //     };
+  //   }
+
+  //   let res = await fetch('/api/posts/filtered', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify(newFilterState),
+  //   });
+
+  //   let data = await res.json();
+
+  //   if (!res.ok) {
+  //     setError(res.error);
+  //     setLoading(false);
+  //   }
+
+  //   if (res.ok) {
+  //     setIsFiltered(true);
+  //     setPageCount(data.pagination.pageCount);
+  //     setPosts(data);
+  //     setError(false);
+  //   }
+  // };
+
+  const fetchPosts = async (filterState, e) => {
+    e?.preventDefault();
+    const res = await fetch('/api/posts', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(filterState),
+      body: JSON.stringify({ page, filterState }),
     });
-
-    let data = await res.json();
+    const data = await res.json();
 
     if (!res.ok) {
-      setError(res.error);
+      setError(true);
       setLoading(false);
     }
 
     if (res.ok) {
       setPosts(data);
+      setLoading(false);
       setError(false);
+      setPageCount(data.pagination.pageCount);
     }
   };
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch('/api/posts');
-      const data = await res.json();
-
-      if (!res.ok) {
-        setError(true);
-        setLoading(false);
-      }
-
-      if (res.ok) {
-        setPosts(data);
-        setLoading(false);
-        setError(false);
-      }
-    };
-
     fetchPosts();
-  }, []);
+  }, [page]);
+
+  function handlePrevious() {
+    setPage((p) => {
+      if (p === 1) return p;
+      return p - 1;
+    });
+  }
+
+  function handleNext() {
+    setPage((p) => {
+      if (p === pageCount) return p;
+      return p + 1;
+    });
+  }
 
   return (
     <main className="flex flex-col items-center">
@@ -67,14 +102,47 @@ function Browse() {
         <section className="text-white mt-8">
           {/* filter */}
           <p className="font-bold text-xl mb-6">Filter by</p>
-          <FilterBar handleSubmit={handleSubmit} />
+          <FilterBar fetchPosts={fetchPosts} />
         </section>
         {/* posts */}
-        <section className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <section>
           {' '}
           {loading && <div>Fetching data...</div>}
           {error && <div>{error}</div>}
-          {posts && posts.map((post) => <Post key={post._id} postRef={post} />)}
+          <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {posts &&
+              posts.posts.map((post) => <Post key={post._id} postRef={post} />)}
+          </div>
+          <div className="flex justify-center items-center mb-20">
+            {page === 1 ? (
+              ''
+            ) : (
+              <button
+                onClick={handlePrevious}
+                className="p-4 mr-4 rounded-md  text-white bg-rose-700 hover:text-slate-900 font-bold cursor-pointer"
+              >
+                Previous
+              </button>
+            )}
+
+            <p className="text-white font-bold text-lg">
+              <span className="text-rose-700 m-2">{page}</span>
+              <span>
+                out of<span className="m-2">{pageCount}</span>
+              </span>
+            </p>
+
+            {page === pageCount ? (
+              ''
+            ) : (
+              <button
+                onClick={handleNext}
+                className="p-4 ml-4 rounded-md  text-white bg-rose-700 hover:text-slate-900 font-bold"
+              >
+                Next
+              </button>
+            )}
+          </div>
         </section>
       </div>
     </main>
