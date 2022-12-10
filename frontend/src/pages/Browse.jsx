37afdacc0f9b56ff8c85529211c54c6943049088
giotsere <giorgitsereteli@protonmail.com>
2022-12-11 01:14:59 +0200

    trying setting credentials browser

--- frontend/src/pages/Browse.jsx
+++ frontend/src/pages/Browse.jsx
@@ -93,8 +93,10 @@ function Browse() {
   //original post fetching and filtered fetching for pagination
   const fetchPosts = async (e) => {
     e?.preventDefault();
-    const res = await fetch('/api/posts', {
+    const res = await fetch('https://valobuddy.onrender.com/api/posts', {
       method: 'POST',
+      withCredentials: true,
+      credentials: 'include',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ page, filterState }),
     });
